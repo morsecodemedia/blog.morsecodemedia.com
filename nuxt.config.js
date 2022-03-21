59476cf9e1cbe0bab7d40c14e5fc8190d6258e68
Brandon Morse <brandon@morsecodemedia.com>
2022-03-21 15:27:23 -0400

    adds html lang attr

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,9 @@ export default {
   */
   head: {
     title: 'Technical writings of Brandon Morse | a morsecodemedia project',
+    htmlAttrs: {
+      lang: 'en'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
